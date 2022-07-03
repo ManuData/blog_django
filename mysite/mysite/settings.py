fc77f7d37d19f8eec7a89e9c7f2c834406a7ac8e
Manuel Vicario Pérez <manuel.vicario.perez@gmail.com>
2022-07-03 23:15:38 +0200

    Secret key in venv

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5b5di@c!ofaayl=yeyxa5s#38rpcymip0!d2%bhjwe%@1)@xvo'
+SECRET_KEY = '...' # NOTA: Guardada en venv
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Incorporamos el app del blog
+    'blog.apps.BlogConfig'
 ]
 
 MIDDLEWARE = [
